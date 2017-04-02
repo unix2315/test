2879a7e23cb65a4d39be70be4e3dce600130bc9b
unix2315 <unix.2315@gmail.com>
2017-04-02 15:47:38 +0300

    ticket:1 Add test for view returned hard-coded template

--- apps/hello/tests/test_views.py
+++ apps/hello/tests/test_views.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from django.test import TestCase
 from django.test import Client
+from django.test import RequestFactory
+from apps.hello.views import home_view
+from django.core.urlresolvers import reverse
 
 
 class HomePageViewTest(TestCase):
@@ -17,3 +20,9 @@ class HomePageViewTest(TestCase):
         """Check, if request to home_page right template"""
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'hello/home_page.html')
+
+    def test_home_view_return_correct_status_code(self):
+        """Check, if edit_view return status code 200"""
+        test_request = RequestFactory().get(reverse('hello:home_page'))
+        test_response = home_view(test_request)
+        self.assertEqual(test_response.status_code, 200)
