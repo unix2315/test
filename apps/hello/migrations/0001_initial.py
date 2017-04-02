8bb32720b8a7c929995c6f7c4d2738a7de2da16d
unix2315 <unix.2315@gmail.com>
2017-04-02 21:45:18 +0300

    ticket:1 add migratons

---
+++ apps/hello/migrations/0001_initial.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Person'
+        db.create_table(u'hello_person', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=30)),
+            ('last_name', self.gf('django.db.models.fields.CharField')(max_length=30)),
+            ('date_of_birth', self.gf('django.db.models.fields.DateField')(null=True)),
+            ('bio', self.gf('django.db.models.fields.TextField')(blank=True)),
+            ('email', self.gf('django.db.models.fields.EmailField')(max_length=75, blank=True)),
+            ('skype', self.gf('django.db.models.fields.CharField')(max_length=30, blank=True)),
+            ('jabber', self.gf('django.db.models.fields.EmailField')(max_length=75, blank=True)),
+            ('other_contacts', self.gf('django.db.models.fields.TextField')(blank=True)),
+        ))
+        db.send_create_signal(u'hello', ['Person'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Person'
+        db.delete_table(u'hello_person')
+
+
+    models = {
+        u'hello.person': {
+            'Meta': {'object_name': 'Person'},
+            'bio': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'date_of_birth': ('django.db.models.fields.DateField', [], {'null': 'True'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'jabber': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '30'}),
+            'other_contacts': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'skype': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['hello']
