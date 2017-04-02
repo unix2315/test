a25ddb68fca899c7cdddcbfc5a2afe14441494f0
unix2315 <unix.2315@gmail.com>
2017-04-02 13:56:07 +0300

    ticket:1 Create home_page template, add tests to url returned this template - FAIL

---
+++ apps/hello/tests/test_views.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from django.test import TestCase
+from django.test import Client
+
+
+class HomePageViewTest(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_request_to_home_page_return_correct_status_code(self):
+        """Check, if request to home_page return status code 200"""
+        test_response = self.client.get('/')
+        self.assertEqual(test_response.status_code, 200)
+
+    def test_request_to_edit_page_uses_proper_template(self):
+        """Check, if request to home_page right template"""
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'hello/home_page.html')
--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -5,5 +5,6 @@ admin.autodiscover()
 
 urlpatterns = patterns(
     '',
+    url(r'', include('apps.hello.urls', namespace='hello')),
     url(r'^admin/', include(admin.site.urls)),
 )
