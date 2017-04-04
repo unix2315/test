183778e1d5efe1e5152847a55145bff5dd1765b8
unix2315 <unix.2315@gmail.com>
2017-04-04 21:03:41 +0300

    ticket:3 add core js module to frontend

---
+++ apps/hello/static/js/requests_module.js
@@ -0,0 +1,39 @@
+var CORE, REQTABLE, AJAXREQ, PAGEHEADERUPDATER;
+CORE = (function(){
+    var moduleData = {};
+    return{
+        //Register module in CORE, to add it to moduleData object
+        registerModule: function(moduleName, that){
+            moduleData[moduleName] = {
+                modObj : that,
+                status : null
+            };
+        },
+        //Init all modules consist in moduleData
+        startAllMod: function(){
+            for (var mod in moduleData){
+                if (moduleData.hasOwnProperty(mod)) {
+                    moduleData[mod].modObj.init();
+                    console.log('init', mod);
+                }
+            }
+        },
+        //Register module-observer at the event of module-publisher
+        registerEvents: function(moduleName, evts){
+            moduleData[moduleName].events = evts;
+        },
+		//Triger some event
+        triggerEvent: function(evt){
+            for (var mod in moduleData){
+                if (moduleData.hasOwnProperty(mod)){
+                    mod = moduleData[mod];
+                    if (mod.events && mod.events[evt.type]){
+                        mod.events[evt.type](evt.data);
+                    }
+                }
+            }
+        }
+    };
+})();
+
+CORE.startAllMod()
