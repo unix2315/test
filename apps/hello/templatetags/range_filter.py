57d76f03a61167c0dbad0e714193d185204ba6b1
unix2315 <unix.2315@gmail.com>
2017-04-04 15:33:37 +0300

    ticket:3 add range_filter templatetags, modify urls.py to return hard-coded requests template

---
+++ apps/hello/templatetags/range_filter.py
@@ -0,0 +1,9 @@
+from django import template
+
+
+register = template.Library()
+
+
+@register.filter
+def create_range(value, start_index=0):
+    return range(start_index, value+start_index)
--- apps/hello/urls.py
+++ apps/hello/urls.py
@@ -1,8 +1,14 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import url
 from apps.hello.views import home_view
+from django.views.generic.base import TemplateView
 
 
 urlpatterns = [
     url(r'^$', home_view, name='home_page'),
+    url(
+        r'^requests/$',
+        TemplateView.as_view(template_name="hello/requests_page.html"),
+        name='requests_page'
+    ),
 ]
