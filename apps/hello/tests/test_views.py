7b83ada05af328b5f0ed5c5a928606dafea1543a
unix2315 <unix.2315@gmail.com>
2017-04-06 11:20:29 +0300

    ticket:3 add views_test return data from model

--- apps/hello/tests/test_views.py
+++ apps/hello/tests/test_views.py
@@ -5,13 +5,19 @@ from django.test import RequestFactory
 from apps.hello.views import home_view, requests_view
 from django.core.urlresolvers import reverse
 from datetime import date
-from apps.hello.models import Person
+from apps.hello.models import Person, RequestsLog
 
 
 PERSON_DATA = {
     'name': 'Alex',
     'last_name': 'Ivanov',
     'date_of_birth': date(1945, 5, 9)
+}
+
+REQUEST_DATA = {
+    'method': 'GET',
+    'path': '/requests/',
+    'status_code': 200
 }
 
 
@@ -74,16 +80,36 @@ class RequestsViewTest(TestCase):
 
     def test_request_page_return_correct_status_code(self):
         """Check, if request to requests_page return status code 200"""
-        self.response = self.client.get(reverse('hello:requests_page'))
-        self.assertEqual(self.response.status_code, 200)
+        test_response = self.client.get(reverse('hello:requests_page'))
+        self.assertEqual(test_response.status_code, 200)
 
     def test_requests_page_uses_proper_template(self):
         """Check, if home_page view render right template"""
-        response = self.client.get(reverse('hello:requests_page'))
-        self.assertTemplateUsed(response, 'hello/requests_page.html')
+        test_response = self.client.get(reverse('hello:requests_page'))
+        self.assertTemplateUsed(test_response, 'hello/requests_page.html')
 
     def test_requests_view_return_correct_status_code(self):
         """Check, if requests_view return status code 200"""
         test_request = RequestFactory().get(reverse('hello:requests_page'))
         test_response = requests_view(test_request)
         self.assertEqual(test_response.status_code, 200)
+
+    def test_requests_view_return_proper_model_instance(self):
+        """Check, if requests view return proper model
+        instance in context"""
+        test_response = self.client.get(reverse('hello:requests_page'))
+        for req in test_response.context['requests']:
+            self.assertIsInstance(req, RequestsLog)
+
+    def test_requests_view_without_data_in_db(self):
+        """Check, if view return correct status, when no data in model"""
+        test_response = self.client.get(reverse('hello:requests_page'))
+        self.assertContains(test_response, "No requests was found")
+
+    def test_requests_view_return_only_last_ten_requests(self):
+        """Check, if requests_view return only last ten requests"""
+        for i in range(20):
+            RequestsLog(**REQUEST_DATA).save()
+        test_response = self.client.get(reverse('hello:requests_page'))
+        self.assertEqual(len(test_response.context['requests']), 10)
+        self.assertContains(test_response, '/requests/', count=10)
