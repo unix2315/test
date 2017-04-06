38fa79ee701916db29b9c03970754f779236a9e6
unix2315 <unix.2315@gmail.com>
2017-04-06 10:48:14 +0300

    ticket:3 add requests_view tests-pass

--- apps/hello/urls.py
+++ apps/hello/urls.py
@@ -1,14 +1,9 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import url
-from apps.hello.views import home_view
-from django.views.generic.base import TemplateView
+from apps.hello.views import home_view, requests_view
 
 
 urlpatterns = [
     url(r'^$', home_view, name='home_page'),
-    url(
-        r'^requests/$',
-        TemplateView.as_view(template_name="hello/requests_page.html"),
-        name='requests_page'
-    ),
+    url(r'^requests/$', requests_view, name='requests_page'),
 ]
--- apps/hello/views.py
+++ apps/hello/views.py
@@ -7,3 +7,7 @@ def home_view(request):
     person = Person.objects.first()
     context = {'person': person}
     return render(request, 'hello/home_page.html', context)
+
+
+def requests_view(request):
+    return render(request, 'hello/requests_page.html')
