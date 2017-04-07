be6b36f85830450a39310330568800ffa7264bef
unix2315 <unix.2315@gmail.com>
2017-04-07 10:07:41 +0300

    ticket:5 add edit_page template add turn it to django

--- apps/hello/urls.py
+++ apps/hello/urls.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import url
 from apps.hello.views import home_view, requests_view
+from django.views.generic.base import TemplateView
 
 
 urlpatterns = [
     url(r'^$', home_view, name='home_page'),
     url(r'^requests/$', requests_view, name='requests_page'),
+    url(r'^edit/$', TemplateView.as_view(template_name="hello/edit_page.html")),
 ]
