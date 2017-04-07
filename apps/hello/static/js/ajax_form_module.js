b6f63c9879f312ea62e571d21e909b268dea92f4
unix2315 <unix.2315@gmail.com>
2017-04-07 13:00:37 +0300

    ticket:5 add block form state and success message

--- apps/hello/static/js/ajax_form_module.js
+++ apps/hello/static/js/ajax_form_module.js
@@ -11,12 +11,34 @@ AJAXFORM = (function(){
         };
 
     function handleFormData(){
+        $("#form_submit_state").hide();
+        blockForm()
     }
     function handleResponseData(data) {
         setTimeout(function () {
             $('#id_submit_btn')
                 .replaceWith('<button id="id_submit_btn" type="submit" class="btn btn-primary">Submit</button>');
+            unblockForm();
+            showSuccessMsg()
         }, 1000);
+    }
+    function unblockForm() {
+            $('#loading_state').hide();
+            $('textarea').removeAttr('disabled');
+            $('input').removeAttr('disabled');
+            $('#id_submit_btn').removeAttr('disabled')
+        }
+    function blockForm() {
+            $("#loading_state").show();
+            $('textarea').attr('disabled', 'disabled');
+            $('input').attr('disabled', 'disabled');
+            $('#id_submit_btn').attr('disabled', 'disabled')
+        }
+    function showSuccessMsg(){
+        $("#form_submit_state").show();
+        setTimeout(function () {
+            $("#form_submit_state").hide()
+        }, 1500)
     }
     return{
         init: function(){
