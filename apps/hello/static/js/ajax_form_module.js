78678cf1810facbfabf8f87b681621fa02b3a309
unix2315 <unix.2315@gmail.com>
2017-04-07 15:42:29 +0300

    ticket:5 add photoupdate method to update photo at frontend

--- apps/hello/static/js/ajax_form_module.js
+++ apps/hello/static/js/ajax_form_module.js
@@ -4,7 +4,12 @@ AJAXFORM = (function(){
     var moduleName = 'AJAXFORM',
         that,
         validOpts,
-        ajaxSubmitOpts;
+        ajaxSubmitOpts,
+        mockData;
+    mockData = {
+        'status': 'OK',
+        'person_photo': "/static/img/test_frontend.jpg"
+    };
     ajaxSubmitOpts = {
         dataType: 'json',
         beforeSubmit: handleFormData,
@@ -27,12 +32,15 @@ AJAXFORM = (function(){
         $("#form_submit_state").hide();
         blockForm()
     }
-    function handleResponseData(data) {
+    function handleResponseData() {
         setTimeout(function () {
             $('#id_submit_btn')
                 .replaceWith('<button id="id_submit_btn" type="submit" class="btn btn-primary">Submit</button>');
             unblockForm();
-            showSuccessMsg()
+            if (mockData['status'] == 'OK') {
+                showSuccessMsg();
+                photoUpdate(mockData)
+            }
         }, 1000);
     }
     function unblockForm() {
@@ -53,18 +61,29 @@ AJAXFORM = (function(){
             $("#form_submit_state").hide()
         }, 1500)
     }
-    validOpts = {
-        rules: {
-            name: "required",
-            last_name: "required",
-            date_of_birth: "required",
-            email: "email",
-            jabber: "email"
-        },
-        submitHandler: function(form) {
-            $(form).ajaxSubmit(ajaxSubmitOpts)
+    function photoUpdate(mockData){
+        var $photoImg = $('#person_img'),
+            $photoClear = $('#photo-clear_id'),
+            $photoInput = $('#id_photo');
+        if ($photoInput.val()!=''){
+            if($photoClear.length){
+                $photoClear.replaceWith('<input id="photo-clear_id" name="photo-clear" type="checkbox" />')
+            }
+            else{
+                $photoInput.before('<input id="photo-clear_id" name="photo-clear" type="checkbox" />' +
+                                '<label for="photo-clear_id">Clear</label>')
+            }
+            $photoImg.replaceWith('<img id="person_img" src="' + mockData['person_photo'] + '">');
+            $photoInput.replaceWith('<input id="id_photo" name="photo" type="file" />')
         }
-    };
+        if ($photoClear.is(':checked')){
+            $photoImg.replaceWith('<img id="person_img" src="/static/img/no_photo.jpg">');
+            if($photoClear.length){
+                $photoClear.next().remove();
+                $photoClear.remove()
+            }
+        }
+    }
     return{
         init: function(){
             $(function(){
