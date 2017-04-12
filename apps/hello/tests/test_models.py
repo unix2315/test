68ddf2d9658ea3d95682203e9b90e648a5333107
unix2315 <unix.2315@gmail.com>
2017-04-12 18:30:36 +0300

    ticket:5 add test for model save method

---
+++ apps/hello/tests/test_models.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+from django.test import TestCase
+from apps.hello.models import Person
+from django.test.utils import override_settings
+from django.core.files.uploadedfile import SimpleUploadedFile
+import os
+from django.conf import settings
+import shutil
+from datetime import date
+
+
+@override_settings(MEDIA_ROOT=settings.MEDIA_TEST_ROOT)
+class PersonModelTest(TestCase):
+
+
+    def setUp(self):
+        self.test_person = Person.objects.first()
+        self.test_img_path = os.path.join(
+            settings.BASE_DIR,
+            'assets/img/test_image.png'
+        )
+        with open(self.test_img_path, 'rb') as test_img:
+            self.test_image_1 = SimpleUploadedFile(
+                name='test_image_1.png',
+                content=test_img.read(),
+                content_type='image/png'
+            )
+        self.test_person.foto = self.test_image_1
+        self.test_person.save()
+        self.first_photo_file = self.test_person.photo.path
+
+    def tearDown(self):
+        if os.path.exists(settings.MEDIA_TEST_ROOT):
+            shutil.rmtree(settings.MEDIA_TEST_ROOT)
+
+    def test_save_method(self):
+        """Check, if model save method,
+         save first person photo to proper filesystem path,
+         and crop image to proper size"""
+        self.assertTrue(os.path.exists(self.first_photo_file))
+        self.assertEqual(
+            self.first_photo_file,
+            settings.MEDIA_TEST_ROOT + self.test_person.photo.name
+        )
+        self.assertTrue(
+            self.test_person.foto.width <= 200 and
+            self.test_person.foto.height <= 200
+        )
+
+    def test_save_method_remove_unused_img(self):
+        """Check, if model save method delete unused images"""
+        with open(self.test_img_path, 'rb') as test_img:
+            self.test_image_2 = SimpleUploadedFile(
+                name='test_image_2.png',
+                content=test_img.read(),
+                content_type='image/png'
+        )
+        self.test_person.foto = self.test_image_2
+        self.test_person.save()
+        self.second_photo_file = self.test_person.photo.path
+        self.assertTrue(os.path.exists(self.second_photo_file))
+        self.assertFalse(os.path.exists(self.first_photo_file))
