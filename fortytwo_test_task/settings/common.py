d2982f87957b9c3d6d83bd85bc1087f7ad643e29
unix2315 <unix.2315@gmail.com>
2017-04-26 18:05:56 +0300

    ticket:13 delete data migrations

--- fortytwo_test_task/settings/common.py
+++ fortytwo_test_task/settings/common.py
@@ -101,6 +101,7 @@ LOGIN_REDIRECT_URL = '/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
+
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
