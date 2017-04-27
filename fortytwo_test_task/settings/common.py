dcfada8ac0ef3e4f9bca5e71b050cb3b15f178c2
unix2315 <unix.2315@gmail.com>
2017-04-27 19:21:02 +0300

    ticket:13 test migrations

--- fortytwo_test_task/settings/common.py
+++ fortytwo_test_task/settings/common.py
@@ -20,7 +20,6 @@ OUTER_DIR = os.path.dirname(BASE_DIR)
 # App/Library Paths
 sys.path.append(os.path.join(BASE_DIR, 'apps'))
 
-# Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
@@ -32,7 +31,6 @@ DEBUG = True
 TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
