954a57d0b4c8b09bef6e525bc7eb2f9cb818ce3f
unix2315 <unix.2315@gmail.com>
2017-05-11 11:49:14 +0300

    modify wsqi

--- fortytwo_test_task/wsgi.py
+++ fortytwo_test_task/wsgi.py
@@ -8,7 +8,20 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
+import sys
 from django.core.wsgi import get_wsgi_application
+from django.conf import settings
+from ws4redis.uwsgi_runserver import uWSGIWebsocketServer
+
+
+sys.path.insert(0, os.path.abspath('..'))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fortytwo_test_task.settings")
 
-application = get_wsgi_application()
+_django_app = get_wsgi_application()
+_websocket_app = uWSGIWebsocketServer()
+
+
+def application(environ, start_response):
+    if environ.get('PATH_INFO').startswith(settings.WEBSOCKET_URL):
+        return _websocket_app(environ, start_response)
+    return _django_app(environ, start_response)
